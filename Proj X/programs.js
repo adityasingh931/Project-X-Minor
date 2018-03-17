d2a1fb3fa1106ea091e1dc64d347a99c068b83a8
AbhiAbzs <abhijeet.abzs@hotmail.com>
2018-03-17 15:49:13 +0530

    Wrote program for Login/Sign-up form and fixed all bugs. Signed-off-by: AbhiAbzs <abhijeet.abzs@hotmail.com>

--- Proj X/programs.js
+++ Proj X/programs.js
@@ -1,12 +1,51 @@
-function initMap() {
+var bussUser = document.getElementById("business-user");
+var workUser = document.getElementById("work-user");
+var lgnFlag = false;  //variable to track state of login form
 
-    var location = { lat: 23.250115, lng: 77.522173 };
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: location
-    });
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
+bussUser.addEventListener("click", function () {
+    if (lgnFlag) {
+        loginView();
+    }
+});
+
+workUser.addEventListener("click", function () {
+    if (lgnFlag) {
+        loginView();
+    }
+});
+
+// form login & sign-up control
+var signup = document.querySelector("#signup");
+var login = document.querySelector("#login");
+var hidden = document.querySelectorAll(".hidden");
+
+signup.addEventListener("click", function () {
+    signupView();
+    lgnFlag = !lgnFlag;
+});
+
+login.addEventListener("click", function () {
+    loginView();
+});
+
+function loginView(){
+    for (var i = 0; i < hidden.length; i++) {
+        hidden[i].classList.add("hidden");
+    }
+    signup.classList.remove("active");
+    signup.classList.remove("btn-info");
+    login.classList.add("btn-info");
+    login.classList.add("active");
+    document.querySelector("#lgn").classList.remove("hidden");
+}
+
+function signupView(){
+    for (var i = 0; i < hidden.length; i++) {
+        hidden[i].classList.remove("hidden");
+    }
+    login.classList.remove("active");
+    login.classList.remove("btn-info");
+    signup.classList.add("btn-info");
+    signup.classList.add("active");
+    document.querySelector("#lgn").classList.add("hidden");
 }
