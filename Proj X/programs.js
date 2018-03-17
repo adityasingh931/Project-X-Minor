9d25fd5c9c012efb3a968a69e1f6e7e397593f52
AbhiAbzs <abhijeet.abzs@hotmail.com>
2018-03-17 16:18:25 +0530

    Fixed Minor Bugs in form toggle button.
    Signed-off-by: AbhiAbzs <abhijeet.abzs@hotmail.com>

--- Proj X/programs.js
+++ Proj X/programs.js
@@ -1,17 +1,12 @@
 var bussUser = document.getElementById("business-user");
 var workUser = document.getElementById("work-user");
-var lgnFlag = false;  //variable to track state of login form
 
 bussUser.addEventListener("click", function () {
-    if (lgnFlag) {
-        loginView();
-    }
+    loginView();
 });
 
 workUser.addEventListener("click", function () {
-    if (lgnFlag) {
-        loginView();
-    }
+    loginView();
 });
 
 // form login & sign-up control
@@ -21,31 +16,32 @@ var hidden = document.querySelectorAll(".hidden");
 
 signup.addEventListener("click", function () {
     signupView();
-    lgnFlag = !lgnFlag;
 });
 
 login.addEventListener("click", function () {
     loginView();
 });
 
-function loginView(){
+function loginView() {
     for (var i = 0; i < hidden.length; i++) {
         hidden[i].classList.add("hidden");
     }
-    signup.classList.remove("active");
+    // signup.classList.remove("active");
     signup.classList.remove("btn-info");
     login.classList.add("btn-info");
-    login.classList.add("active");
+    // login.classList.add("active");
     document.querySelector("#lgn").classList.remove("hidden");
+    document.querySelector(".check  ").classList.remove("hidden");
 }
 
-function signupView(){
+function signupView() {
     for (var i = 0; i < hidden.length; i++) {
         hidden[i].classList.remove("hidden");
     }
-    login.classList.remove("active");
+    // login.classList.remove("inactive");
     login.classList.remove("btn-info");
     signup.classList.add("btn-info");
-    signup.classList.add("active");
+    // signup.classList.add("active");
     document.querySelector("#lgn").classList.add("hidden");
+    document.querySelector(".check").classList.add("hidden");
 }
