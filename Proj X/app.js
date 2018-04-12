7e7b54d192084610b887b18f2051bf677e283c47
AbhiAbzs <abhijeet.abzs@hotmail.com>
2018-04-12 19:05:46 +0530

    Experiments going on

--- Proj X/app.js
+++ Proj X/app.js
@@ -4,15 +4,23 @@ var app = express();
 
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static("images"));
+app.use(express.static("styles"));
+app.use(express.static("scripts"));
 
-//routes logics
+//Start of Routes Logics
+
 app.get("/", function (req, res) {
     res.render("home");    
 });
 
+app.get("*", function(req, res){
+    res.render("error");
+});
+
+//End of Routes Logic
 
 //starting server and starting listener
-app.listen(process.env.PORT, process.env.ID, function(){
-    console.log("Starting listening to the server...");
+app.listen(2000, function(){
+    console.log("Started listening to the server @ port 2000...");
 });
